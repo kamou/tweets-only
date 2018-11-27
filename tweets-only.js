a2e0b2edfc613f04eb3a3e3d4fe5d39e77c21f9f
Ayman Khamouma <ak42@mg.blackbunny.io>
2018-11-27 21:14:39 +0100

    Use MutationObserver to detect timeline updates

--- tweets-only.js
+++ tweets-only.js
@@ -1,12 +1,23 @@
-function deletePromoted()
-{
-    promoted = document.getElementsByClassName("js-promoted-badge")
-    for (i = 0; i < promoted.length; i++) {
-        if (promoted[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.className.includes("js-stream-item stream-item stream-item")) {
-            promoted[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove()
+
+var timeline = document.getElementById('stream-items-id')
+
+var config = { childList: true, subtree: false };
+
+var callback = function(mutationsList, observer) {
+    for(var mutation of mutationsList) {
+        if (mutation.type == 'childList') {
+            promoted = document.getElementsByClassName("js-promoted-badge")
+            for (i = 0; i < promoted.length; i++) {
+                promoted[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove()
+                console.log('Removed promoted tweet.');
+            }
         }
     }
-    clearInterval(tmr)
-    return;
-}
-var tmr = setInterval(deletePromoted, 2000);
+};
+
+// Create an observer instance linked to the callback function
+var observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(timeline, config);
+
